cbb91c5fd780ede841dea1f19df9d8bede611735
xqweak <xqweak@gmail.com>
2023-09-08 13:00:01 -0500

    updates from pwnlap on vie 08 sep 2023 13:00:01 -05

---
+++ fast_scripts/screenright.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x192 --rotate normal --output HDMI-1 --mode 1280x960 --pos 1366x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
