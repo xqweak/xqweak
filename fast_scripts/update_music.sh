3d9caedf44d47241ea2fb8c4d9c4423753bf0b5c
xqweak <xqweak@gmail.com>
2023-10-02 01:30:01 -0500

    updates from pwnlap on lun 02 oct 2023 01:30:01 -05

--- fast_scripts/update_music.sh
+++ fast_scripts/update_music.sh
@@ -6,7 +6,7 @@ update_playlist() {
 	url=$2 # Url of the youtube playlist
 	mkdir -p "$HOME/Music/$name"  && echo ''
 	# Download the playlist
-	yt-dlp $url -x --audio-format mp3 --download-archive $HOME/Music/$name/songs.txt -o "$HOME/Music/$name/%(title)s.%(ext)s" --add-metadata --postprocessor-args "-metadata Album=$name"  # Download the playlist in mp3 or update it
+	yt-dlp $url -x --audio-format mp3 --download-archive $HOME/Music/$name/songs.txt -o "$HOME/Music/$name/%(title)s.%(ext)s" --postprocessor-args "-map_metadata -1 -metadata Album='$name' -metadata album='$name' -metadata artist='xqweak'"
 	echo "done"
 }
 
@@ -14,4 +14,4 @@ update_playlist() {
 # Playlists ...
 update_playlist "Hack" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgW6qCUj9u5HL7491J8tlESC"
 update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62kZ7oi3IzMSCnltc0S1u9LT"
-update_playlist "Musica_toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62n4lyQ2ODCC9nP-rOTTXWvw"
+update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62n4lyQ2ODCC9nP-rOTTXWvw"
