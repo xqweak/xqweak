08215650f62f81bc66959065b5545edfbfb561d2
xqweak <xqweak@gmail.com>
2023-10-01 23:10:46 -0500

    programa para actualizar musica hecho

--- fast_scripts/update_music.sh
+++ fast_scripts/update_music.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-
-lol(){
-path=$1
-echo $path
+# Function for creating a playlist
+create_playlist() {
+	name=$1 # Name of the playlist
+	url=$2 # Url of the youtube playlist
+	mkdir -p "$HOME/Music/$name" && echo "created $HOME/Music/$name"
+	# Download the playlist
+	yt-dlp $url -x --audio-format mp3 --download-archive $HOME/Music/$name/songs.txt -o "$HOME/Music/$name/%(title)s.%(ext)s" # Download the playlist in mp3 or update it
 }
 
-# Define the playlists
-mkdir -p ~/Music/Musica_Toda
-mkdir -p ~/Music/Hack
 
-# Download de playlists
-#
-lol "holas"
+# Playlists ...
+
