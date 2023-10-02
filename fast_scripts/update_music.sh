7704adec7847635898c45266640badab791497b7
xqweak <xqweak@gmail.com>
2023-10-01 23:20:56 -0500

    updated music

--- fast_scripts/update_music.sh
+++ fast_scripts/update_music.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Function for creating a playlist
-create_playlist() {
+update_playlist() {
 	name=$1 # Name of the playlist
 	url=$2 # Url of the youtube playlist
-	mkdir -p "$HOME/Music/$name" && echo "created $HOME/Music/$name"
+	mkdir -p "$HOME/Music/$name"  && echo ''
 	# Download the playlist
 	yt-dlp $url -x --audio-format mp3 --download-archive $HOME/Music/$name/songs.txt -o "$HOME/Music/$name/%(title)s.%(ext)s" # Download the playlist in mp3 or update it
+	echo "done"
 }
 
 
 # Playlists ...
-
+update_playlist "Hack" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgW6qCUj9u5HL7491J8tlESC"
