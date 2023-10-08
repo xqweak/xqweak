0db412900a084b386fa41c71aa60ec7a11bd9273
xqweak <xqweak@gmail.com>
2023-10-08 14:30:02 -0500

    updates from pwnlap on dom 08 oct 2023 14:30:02 -05

--- fast_scripts/update_music.sh
+++ fast_scripts/update_music.sh
@@ -13,5 +13,6 @@ update_playlist() {
 
 # Playlists ...
 update_playlist "Hack" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgW6qCUj9u5HL7491J8tlESC"
+update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgVmX3gZLXH5bRfglWuldzdW"
 update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62kZ7oi3IzMSCnltc0S1u9LT"
 update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62n4lyQ2ODCC9nP-rOTTXWvw"
